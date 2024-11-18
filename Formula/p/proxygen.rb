392d5ea71a487aa81609633c23c58b630c96e151
Michael Cho <michael@michaelcho.dev>
2024-11-18 13:01:39 -0500

    proxygen: revision bump (boost 1.87.0)

--- Formula/p/proxygen.rb
+++ Formula/p/proxygen.rb
@@ -4,6 +4,7 @@ class Proxygen < Formula
   url "https://github.com/facebook/proxygen/releases/download/v2024.12.02.00/proxygen-v2024.12.02.00.tar.gz"
   sha256 "536c5baaf372a590e848d1bf60b46195bec421e2f940034530e1c170d43c4947"
   license "BSD-3-Clause"
+  revision 1
   head "https://github.com/facebook/proxygen.git", branch: "main"
 
   bottle do
